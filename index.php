730d5abb56486277a98dc0001ab730f31f4990a3
Cody Hill <william.cody.hill@gmail.com>
2020-05-05 22:46:19 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Example</title>
+    </head>
+    <body>
+
+        <?php
+            echo "<p>Doesn't PHP seem like the funnest langauge to learn?
+            As far as I understand, PHP is an extension of the Apache webserver.
+            Downloading XAMPP and getting it up and running was a pain, but a good
+            taste of what things in the real programming world will be like.
+            I want to learn PHP to challenge myself, I want to learn it to make server-
+            side rendered web applications and procress data easier. I want to be able
+            to achieve CRUD operations easily and pass around data with multiple SQL
+            servers. Honestly I want to pad my resume with PHP but I also want to see 
+            what useful automations I can achieve with it as well. Specificlly, three
+            projects I want to try are: a GUI, a Facebook application, and a 
+            photo gallery.</p>";
+        ?>
+
+    </body>
+</html>
